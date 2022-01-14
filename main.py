c8f9566b4c2863d8c28c58b1620804378b651322
Niklaas Cotta <nikcotta@gmail.com>
2022-01-13 16:08:17 -0800

    added minor functionality

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Created: 01/13/22
 
 import discord
 import os
+import time
 from dotenv import load_dotenv
 
 client = discord.Client()
@@ -14,6 +15,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
+    await client.change_presence(activity=discord.Activity(name='Jeepers Creepers', type=discord.ActivityType.listening))
 
 
 @client.event
@@ -21,8 +23,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if message.content.startswith("$help"):
+        await message.channel.send("My prefix is ``$``\n")
+        time.sleep(0.5)
+        await message.channel.send("Here is a list of the following commands:\n"
+                                   "``about``\t\t get information about me!\n"
+                                   "``roll <dice> <number>``\t\t roll some dice!\n")
+    elif message.content.startswith("$about"):
+        await message.channel.send("I am a D&D bot created by schnibs ;)")
+    else:
+        await message.channel.send("Command not recognized...")
 
 
 if __name__ == '__main__':
